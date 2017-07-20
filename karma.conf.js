5f61b604904c0752ae6f7af5bb2865b7ca801054
jcon01 <jon.connolly71@gmail.com>
2017-07-20 13:02:25 -0700

    new packaging files

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+//jshint strict: false
+module.exports = function(config) {
+  config.set({
+
+    basePath: './app',
+
+    files: [
+      '**/*.module.js',
+      '*!(.module|.spec).js',
+      '!(bower_components)/**/*!(.module|.spec).js',
+      '**/*.spec.js'
+    ],
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome', 'Firefox'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine'
+    ]
+
+  });
+};
